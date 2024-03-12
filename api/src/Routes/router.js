2b6985ddc421bd8fed691d513b12a491b67321eb
Maël CHAUMONT <mael.chaumont@epitech.eu>
2024-03-12 10:52:35 +0100

    Ajout swagger-ui pour l'api

--- api/src/Routes/router.js
+++ api/src/Routes/router.js
@@ -4,7 +4,9 @@ const express = require("express")
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET");
     res.send('hello world')
 })
 
