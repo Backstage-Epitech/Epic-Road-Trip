667b788ed3760c42f594cc67e45baa4eaa457330
bilal.abd <bilal.abderrahmane69@gmail.com>
2024-03-18 10:25:54 +0100

    fix login + signup

--- api/src/Routes/router.js
+++ api/src/Routes/router.js
@@ -1,6 +1,7 @@
 console.clear()
 
-const express = require("express")
+const express = require("express");
+const { signup, login } = require("../Controllers/userController");
 
 const router = express.Router()
 
@@ -8,6 +9,8 @@ router.get('/', (req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET");
     res.send('hello world')
-})
+});
+router.post('/signup', signup);
+router.post('/login', login);
 
 module.exports = router
